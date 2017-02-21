996fa9d8998ac1d1098b9bdbddf7c89f1555c8f7
samantha <abeaman82@yahoo.com>
2017-02-20 20:11:01 -0500

    01-run.sh

--- 01-run.sh
+++ 01-run.sh
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-#hello everyone
-# Oh Sister evelyn
-=======
-Hi
+hello everyone
 sisters and brothers
-Be good to one another.
- 
->>>>>>> cdc146cf0ce253140f2d6525ff8f5e2e87882476
+Be good to one another
 >>>>>>> 30140091cbf2755e73dd48b42976d20c203cbb32
 
 #1 /bin/bash
